7ef63415965b6297358aa91fb691a20368c026d9
doktoric <doktoricsrichard@gmail.com>
2014-05-02 19:45:34 +0200

    locations get

--- src/main/groovy/com/sequenceiq/cloud/azure/client/AzureClient.groovy
+++ src/main/groovy/com/sequenceiq/cloud/azure/client/AzureClient.groovy
@@ -1,33 +1,69 @@
 package com.sequenceiq.cloud.azure.client
-
+import com.thoughtworks.xstream.io.HierarchicalStreamReader
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter
+import com.thoughtworks.xstream.io.copy.HierarchicalStreamCopier
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
+import com.thoughtworks.xstream.io.xml.XppReader
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
-import groovyx.net.http.RESTClient
+
+import javax.xml.stream.XMLStreamException
 /**
  * Azure cloud REST client - http://msdn.microsoft.com/library/azure/ee460799.aspx
  */
 class AzureClient {
+    static String subscriptionId = "subscriptionId";
+    static String keyStorePath = "WindowsAzureKeyStore.jks";
+    static String keyStorePassword = "password";
+    def slurper = new JsonSlurper();
 
-	def slurper = new JsonSlurper();
-	def RESTClient azure
-	def subscriptionId
-	boolean debugEnabled = false;
-	
-	def String getSubscriptionId() {
-		subscriptionId
-	}
-	
-	def setSubscriptionId(subscriptionId) {
-		this.subscriptionId = subscriptionId
-	}
-	public static void main(String[] args) {
+    boolean debugEnabled = false;
 
-		if (args.size == 1) {
-			setSubscriptionId( args[0])
-			
-		}
+    def String getSubscriptionId() {
+        subscriptionId
+    }
 
-		AzureClient client = new AzureClient()
-		
+    def setSubscriptionId(subscriptionId) {
+        this.subscriptionId = subscriptionId
+    }
 
-	}
+    static void main(String[] args) {
+        def slurper = new JsonSlurper();
+        if (args.size == 1) {
+            setSubscriptionId(args[0]);
+        }
+
+        //List locations
+        String jsonResponse = convert(getLocations())
+        if(jsonResponse != null) {
+            System.out.println(JsonOutput.prettyPrint(jsonResponse));
+        }
+
+    }
+
+    static String getLocations() {
+        String url = String.format("https://management.core.windows.net/%s/locations", subscriptionId);
+        String response = ServiceManagementHelper.processGetRequest(new URL(url), keyStorePath, keyStorePassword);
+        return response;
+    }
+
+    static String convert(String response) throws XMLStreamException, IOException {
+        try {
+            String xmlHeader= "xmlns=\"http://schemas.microsoft.com/windowsazure\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"";
+            HierarchicalStreamReader sourceReader = new XppReader(new StringReader(response.toString().replaceAll(xmlHeader, "")));
+
+            StringWriter buffer = new StringWriter();
+            JettisonMappedXmlDriver jettisonDriver = new JettisonMappedXmlDriver();
+            jettisonDriver.createWriter(buffer);
+            HierarchicalStreamWriter destinationWriter = jettisonDriver.createWriter(buffer);
+
+            HierarchicalStreamCopier copier = new HierarchicalStreamCopier();
+            copier.copy(sourceReader, destinationWriter);
+           return buffer.toString();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage())
+            return null;
+        }
+
+    }
 }
--- src/main/java/com/sequenceiq/cloud/azure/client/ServiceManagementHelper.java
+++ src/main/java/com/sequenceiq/cloud/azure/client/ServiceManagementHelper.java
@@ -1,21 +1,26 @@
+package com.sequenceiq.cloud.azure.client;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.*;
-import java.security.*;
-import java.io.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
+
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import org.apache.commons.codec.binary.Base64;
 
 public class ServiceManagementHelper {
 
-    private static KeyStore getKeyStore(String keyStoreName, String password) throws IOException
-    {
+    private static KeyStore getKeyStore(String keyStoreName, String password) throws IOException {
         KeyStore ks = null;
         FileInputStream fis = null;
         try {
@@ -28,8 +33,7 @@ public class ServiceManagementHelper {
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             if (fis != null) {
                 fis.close();
             }
@@ -42,10 +46,23 @@ public class ServiceManagementHelper {
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
         keyManagerFactory.init(ks, password.toCharArray());
 
-          SSLContext context = SSLContext.getInstance("TLS");
-          context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
 
-          return context.getSocketFactory();
+        return context.getSocketFactory();
+    }
+
+    private static String processGetRequest(URL url, String keyStore, String keyStorePassword) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, IOException {
+        SSLSocketFactory sslFactory = getSSLSocketFactory(keyStore, keyStorePassword);
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setSSLSocketFactory(sslFactory);
+        con.setRequestMethod("GET");
+        con.addRequestProperty("x-ms-version", "2012-03-01");
+        con.addRequestProperty("ContentType", "application/json");
+        InputStream responseStream = (InputStream) con.getContent();
+        String response = getStringFromInputStream(responseStream);
+        responseStream.close();
+        return response;
     }
 
     private static String getStringFromInputStream(InputStream is) {
@@ -76,4 +93,20 @@ public class ServiceManagementHelper {
         return sb.toString();
 
     }
-  }
+
+    public static void main(String[] args) throws IOException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+
+
+        String subscriptionId = "a8b8dac0-2b84-41da-a08b-1e45297e0d2b";
+        String keyStorePath = "//Users//ricsi//prj//WindowsAzureKeyStore.jks";
+        String keyStorePassword = "test123";
+        String url = "";
+
+        //List locations
+        url = String.format("https://management.core.windows.net/%s/locations", subscriptionId);
+        String response = processGetRequest(new URL(url), keyStorePath, keyStorePassword);
+        System.out.println(response);
+
+
+    }
+}
