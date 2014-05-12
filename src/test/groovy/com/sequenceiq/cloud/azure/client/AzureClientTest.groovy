ea94a051480194ff662b8d720b48e6546c9acaa7
doktoric <doktoricsrichard@gmail.com>
2014-05-12 15:18:49 +0200

    added first testcase

---
+++ src/test/groovy/com/sequenceiq/cloud/azure/client/AzureClientTest.groovy
@@ -0,0 +1,39 @@
+package com.sequenceiq.cloud.azure.client
+
+import spock.lang.Specification
+
+class AzureClientTest extends Specification {
+
+    // fill all data if you want to test your changes
+    final String subscriptionId = "subscriptionId"
+    final String keyStorePath = "keyStorePath"
+    final String keyStorePassword = "keyStorePassword"
+    final String clusterName = "azureRestTest123"
+    AzureClient azureClient
+
+    def setup() {
+        azureClient = new AzureClient(subscriptionId, keyStorePath, keyStorePassword)
+        deleteResources()
+    }
+
+    def cleanup() {
+        deleteResources()
+    }
+
+    def deleteResources() {
+        try {
+            azureClient.deleteAffinityGroup(name: clusterName)
+        } catch (e) {
+            println('Resource not exist')
+        }
+    }
+
+
+    void "T001: create affinity group test"() {
+        when:
+        def response = azureClient.createAffinityGroup(name: clusterName, description: 'test group', location: 'East US')
+        then:
+        assert response.isSuccess() == true
+    }
+
+}
