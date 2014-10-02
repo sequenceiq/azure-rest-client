cd0b608f6e34da8cbea43dde81371995defa74bb
Richard Doktorics <doktoricsrichard@gmail.com>
2014-10-02 15:23:17 +0200

    project in gradle

---
+++ src/main/groovy/com/sequenceiq/cloud/azure/main/AzureClientMain.groovy
@@ -0,0 +1,10 @@
+package com.sequenceiq.cloud.azure.client
+
+import com.sequenceiq.cloud.azure.client.AzureClient
+
+class AzureClientMain {
+    public static void main(String[] args) {
+        def client = new AzureClient()
+    }
+
+}
